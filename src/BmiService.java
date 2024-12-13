615396512c0642fc1d30795fa9f02f48658ee9d6
natakumtest <nkumasinskaya@gmail.com>
2024-12-14 01:51:12 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,14 @@
+public class BmiService {
+
+    public double calculate(double height, double weight) {
+
+        double BMI;
+        BMI = weight / (height * height);
+        return BMI;
+        }
+
+
+    }
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+
+        System.out.println();
+        System.out.println("Рост: 1,87 метров ");
+        System.out.println("Вес: 98 кг ");
+        System.out.println("Ваш индекс массы тела: ");
+        System.out.print(service.calculate(1.87, 98));
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Рост: 1,73 метра ");
+        System.out.println("Вес: 51 кг ");
+        System.out.println("Ваш индекс массы тела: ");
+        System.out.print(service.calculate(1.73, 51));
+        System.out.println();
+
+    }
+}
